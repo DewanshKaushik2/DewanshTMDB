92c12554f8c0e23b71a450ac7256aa4758d279e0
dewanshkaushik <kaushik.dewansh1@gmail.com>
2024-12-17 17:48:33 +0530

    Initial commit

--- app/src/main/java/com/tmdbapp/viewmodels/MyDetailsViewModel.java
+++ app/src/main/java/com/tmdbapp/viewmodels/MyDetailsViewModel.java
@@ -6,7 +6,8 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.tmdbapp.data.DataRepository;
 import com.tmdbapp.models.MovieModel;
-
+//git remote add origin https://github.com/DewanshKaushik2/DewanshTMDB.git
+//git remote set-url origin https://github.com/DewanshKaushik2/DewanshTMDB.git
 public class MyDetailsViewModel extends ViewModel {
     private final DataRepository repository;
     private final MediatorLiveData<MovieModel> mediator = new MediatorLiveData<>();
